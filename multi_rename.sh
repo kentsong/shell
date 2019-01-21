dfb57a464dbe05d4f3d7af7c3c3576b8eb5a629c
zhukai.song <songzhukai@tianchimedia.com>
2019-01-21 18:00:10 +0800

    first commit

---
+++ multi_rename.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#更改目录档名
+# params1 目录位置
+# params2 附档名
+
+#example：
+#$. multi_rename.sh /Users/kent/Desktop/201901发票/超市 pdf
+
+i=i;
+path=$1;
+subfix=$2;
+
+for filename in $path/*.$subfix;
+do
+   echo  "$filename";
+   echo "$i.$subfix";
+   mv "$filename" "$path/$i.$subfix";
+   i=`expr $i + 1`;
+done
